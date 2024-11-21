e916ae178e5649a36ea919201fe714327c605fd8
Kevin Arias <david.ap1022@gmail.com>
2024-11-20 19:10:18 -0500

    Codigo saludar actualizado

---
+++ codigoSaludar.cs
@@ -0,0 +1,15 @@
+
+string nombre = "Kevin";
+string apellido = "Roberto";
+string saludo = saludar(nombre, apellido);
+Console.WriteLine(saludo);
+
+public string saludar(nombre, apellido){
+    string saludo = "Hola " + nombre + " " + apellido;
+    return saludo;
+}
+
+
+
+
+
