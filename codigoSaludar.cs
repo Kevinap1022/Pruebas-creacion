e15590641b004eb7a67b88a6c104b4fe2fc56ec5
Kevin Arias <93870819+Kevinap1022@users.noreply.github.com>
2024-11-20 20:08:53 -0500

    Actualizacioncodigo codigoSaludar.cs

--- codigoSaludar.cs
+++ codigoSaludar.cs
@@ -1,11 +1,11 @@
 
 string nombre = "Kevin";
-string apellido = "Gustavo";
+string apellido = "Castaño Pereira";
 string saludo = saludar(nombre, apellido);
 Console.WriteLine(saludo);
 
 public string saludar(nombre, apellido){
-    string saludo = "Hola " + nombre + " " + apellido;
+    string saludo = "Hola buenos dias " + nombre + " " + apellido;
     return saludo;
 }
 
